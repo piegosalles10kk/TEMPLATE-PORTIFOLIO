4559f9e101a3ce486515300face4ff1445f55753
unknown <diegaosalles2012@gmail.com>
2025-09-27 18:19:10 -0300

    <Correcao-url-backend3>

--- backend/src/config/dBConnect.js
+++ backend/src/config/dBConnect.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
-    let dbURI = '';
-    const DB_USER = process.env.MONGO_USER;
-    const DB_PASS = process.env.MONGO_PASS;
-    const DB_HOST = 'mongo-db'; 
-    const DB_PORT = 27017;
-    const DB_NAME = 'projetos';
 
-    if (DB_USER && DB_PASS && DB_HOST && DB_PORT && DB_NAME) {
-        dbURI = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-    } else {
-        dbURI = 'mongodb://localhost:27017/projetos';
-    }
-
-const connectDB = async () => { 
-
+// Função de conexão (DEVE USAR A VARIÁVEL DE AMBIENTE)
+const connectDB = async () => {
     try {
-        await mongoose.connect(dbURI); 
-        console.log("Conectado ao MongoDB Local!");
-    } catch (err) {
-        console.error("Erro ao se conectar ao MongoDB Local:", err);
-        process.exit(1); 
+        const mongoUri = process.env.MONGO_URI; 
+
+        if (!mongoUri) {
+            console.error("Variável de ambiente MONGO_URI não está definida!");
+            return;
+        }
+
+        await mongoose.connect(mongoUri, {
+            
+        });
+        console.log('Conectado ao MongoDB!');
+        
+    } catch (error) {
+
+        console.error('Erro ao se conectar ao MongoDB Local:', error); 
+
     }
-};
+}
 
 module.exports = connectDB;
